57c5cceced1cbe94d33fe99f930d4051f6f94857
Dante Harasz <danteharasz@gmail.com>
2020-07-21 17:30:45 -0400

    trying to integrate node and mongo

--- src/index.jsx
+++ src/index.jsx
@@ -9,8 +9,19 @@ import { App } from './app';
 import './styles.less';
 
 // setup fake backend
-import { configureFakeBackend } from './_helpers';
-configureFakeBackend();
+// import { configureFakeBackend } from './_helpers';
+// configureFakeBackend();
+
+// setup real backend
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://danteharasz@gmail.com:KHsykCcW8VRrnRg@emailsignup.xvh2f.mongodb.net/EmailSignUp?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 
 // attempt silent token refresh before startup
 accountService.refreshToken().finally(startApp);
